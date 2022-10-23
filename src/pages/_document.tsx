14d91426ab229a10adb8714c2b592c0f93ecae71
Eric Viana <ericvsdefault@gmail.com>
2022-10-23 00:25:01 -0300

    spelling check

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* Add external liks */}
+          {/* Add external links */}
         </Head>
 
         <body>
