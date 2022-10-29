3a910ae5ca77cbf02f611ea5f252aa1b408ef359
Eric Viana <ericvsdefault@gmail.com>
2022-10-28 22:03:47 -0300

    Change componet name

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,38 +1,34 @@
-import {
-  Card,
-  ProjectName,
-  ProjectDescription,
-  ProjectInfos,
-  ProjectButton,
-  InfoText,
-} from "./styles";
-import { AiOutlineUsergroupAdd, AiOutlineEye } from "react-icons/ai";
+import { 
+    Card,
+    ProjectName, 
+    ProjectDescription, 
+    ProjectInfos, 
+    ProjectButton, 
+    InfoText 
+} from "./styles"
+import { AiOutlineUsergroupAdd, AiOutlineEye } from 'react-icons/ai'
 
 export function CardComponent() {
   return (
     <Card>
-      <ProjectName>Project Name</ProjectName>
+      <ProjectName >Project Name</ProjectName>
       <ProjectDescription>
-        Plorizzle ipsizzle dolizzle sit amet, yo adipiscing elit. Nullam boofron
-        check it out, volutpizzle.
+      Plorizzle ipsizzle dolizzle sit amet, yo adipiscing elit. Nullam boofron check it out, volutpizzle.
       </ProjectDescription>
 
       <ProjectInfos>
-        <AiOutlineUsergroupAdd
-          size={28}
-          fontWeight="700"
-          color="#AEAEB2"
-        ></AiOutlineUsergroupAdd>
+        <AiOutlineUsergroupAdd size={28} fontWeight='700' color="#AEAEB2"></AiOutlineUsergroupAdd>
         <InfoText>127</InfoText>
-        <AiOutlineEye size={28} fontWeight="700" color="#AEAEB2"></AiOutlineEye>
+        <AiOutlineEye size={28} fontWeight='700' color="#AEAEB2"></AiOutlineEye>
         <InfoText>34</InfoText>
       </ProjectInfos>
+
       <ProjectInfos style={{ padding: "6px 0 0 0px" }}>
-        <InfoText style={{ padding: "0px 0px 0px 2px" }}>Contributors</InfoText>
+      	<InfoText style={{ padding: "0px 0px 0px 2px" }}>Contributors</InfoText>
         <InfoText style={{ padding: "0px 0px 0px 22px" }}>Watching</InfoText>
       </ProjectInfos>
 
-      <ProjectButton href="#">Check it out ›</ProjectButton>
+      <ProjectButton href="#">Check it out  ›</ProjectButton>
     </Card>
-  );
+  )
 }
--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,50 +1,51 @@
 import styled from "styled-components";
 
 export const Card = styled.div`
+  width: 388px;
+  height: 368px;
 
-width: 388px;
-height: 368px;
+  background: var(--bg-content);
+  border-radius: 36px;
 
-background: #F8F8F8;
-border-radius: 36px;
-
-display: flex;
-flex-direction: column;
-padding: 61px
+  display: flex;
+  flex-direction: column;
+  padding: 61px;
 `
 export const ProjectLogo = styled.a`
   padding: 16px 0 0 0;
 `
 
 export const ProjectName = styled.h3`
-  font-size: 18px;
+  font-size: 1.12rem;
   
   padding: 0;
   font-weight: 800;
-  color: #2C2C2E;
+  color: var(--title-color);
 `
 
 export const ProjectDescription = styled.span`
-  font-size: 16px;
+  font-size: 1rem;
   padding: 16px 0 0 0;
   font-weight: 600;
-  color: #333333;
+  color: var(--subtitle-color);
 `
+
 export const ProjectInfos = styled.div`
   display: flex;
-  padding: 30px 0px 0px 0px;
+  padding: 30px 0 0 0;
   align-items: center;
-  `
+`
+
 export const InfoText = styled.span`
   color: var(--gray);
   padding: 4px 52px 0px 10px;
-  font-size: 16px;
+  font-size: 1rem;
   font-weight: 600;
-  line-height: 144.2%;
 `
+
 export const ProjectButton = styled.a`
-  padding: 40px 0 60px 0;
+  margin: 40px 0 60px 0;
   color: var(--blue);
-  font-size: 16px;
+  font-size: 1rem;
   font-weight: 700;
 `
